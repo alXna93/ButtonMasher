f2d1bcb2ef787fe9ebc041c4bf8d169b075c6bbd
alXna93 <43002121+alXna93@users.noreply.github.com>
2018-09-05 15:38:33 +0100

    setup project for SFML

---
+++ Main.cpp
@@ -0,0 +1,25 @@
+//included libraries
+
+#include <SFML/Graphics.hpp>
+
+
+//entry point for program
+int main()
+{
+	//Make a variable called gameWindow for type RenderWindow
+	sf::RenderWindow gameWindow;
+
+	//Game Loop
+	while (true)
+	{
+		
+		//TODO: Check for input
+
+		//TODO: Update game state
+
+		//TODO: Draw graphics
+	}
+
+	//exit point for program
+	return 0;
+}
