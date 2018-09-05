2de98a758cf3061d138dfdacace9674bd40fae35
alXna93 <43002121+alXna93@users.noreply.github.com>
2018-09-05 15:58:46 +0100

    created full size window

--- Main.cpp
+++ Main.cpp
@@ -8,12 +8,28 @@ int main()
 {
 	//Make a variable called gameWindow for type RenderWindow
 	sf::RenderWindow gameWindow;
+	gameWindow.create(sf::VideoMode::getDesktopMode(), "Button Masher", 
+		sf ::Style::Titlebar | sf::Style::Close);
+
 
 	//Game Loop
-	while (true)
+	//Runs every frame until the game window is closed
+	while (gameWindow.isOpen())
 	{
 		
-		//TODO: Check for input
+		//Check for input
+		sf::Event gameEvent;
+		while (gameWindow.pollEvent(gameEvent))
+		{
+			//process events
+
+			//check if the event is the closed event
+			if (gameEvent.type == sf::Event::Closed)
+			{
+				gameWindow.close();
+			}
+		}
+			
 
 		//TODO: Update game state
 
